2fea4a13b89f3c26b6581c036cef6d57f560abf8
Melaine Gérard <pro@melaine-gerard.fr>
2026-03-15 12:01:00 +0100

    :lipstick: bump app version

--- lib/service/menu_service.dart
+++ lib/service/menu_service.dart
@@ -85,8 +85,8 @@ class MenuService {
     showAboutDialog(
       context: context,
       applicationName: 'Simple Notepad',
-      applicationVersion: '0.1.3',
-      applicationLegalese: '© 2025 - Melaine Gérard',
+      applicationVersion: '0.1.5',
+      applicationLegalese: '© 2026 - Melaine Gérard',
       applicationIcon: const Icon(Icons.note),
     );
   }
